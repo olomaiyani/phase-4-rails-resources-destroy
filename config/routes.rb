74486de19a3b57ea0542908f6ad51a3cd67d2149
Nana <nanaolomaiyani@gmail.com>
2022-11-25 14:24:02 +0300

    assignment done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show, :create, :update]
   patch "/birds/:id/like", to: "birds#increment_likes"
+
+  resources :birds, only: [:index, :show, :create, :update, :destroy]
+
 end
